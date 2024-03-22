7c92e5d478ab1e0ff9b5264ae8035f64a938a435
Jason Yapri <jason.yapri@gmail.com>
2024-03-22 13:55:38 +0700

    Renamed Counter.sol to NFTMarketplace.sol

---
+++ test/NFTMarketplace.t.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.25;
+
+import {Test, console} from "forge-std/Test.sol";
+import {NFTMarketplace} from "../src/NFTMarketplace.sol";
+
+contract NFTMarketplaceTest is Test {}
